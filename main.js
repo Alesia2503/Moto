6ecf40fec62e92c077593405c2af12735fd2dcf3
Alesia_Shikurova <ales2503@yandex.by>
2024-04-03 15:51:03 +0300

    fix: new version

--- main.js
+++ main.js
@@ -25,19 +25,63 @@ items.forEach((item, index) => {
 
 
 // Telegram channel @FrontendPortal
-const boxes = document.querySelectorAll('.glass');
-window.addEventListener('scroll', checkBoxes);
-checkBoxes();
+// const boxes = document.querySelectorAll('.glass');
+// window.addEventListener('scroll', checkBoxes);
+// checkBoxes();
 
-function checkBoxes() {
-	const triggerBottom = window.innerHeight / 5 * 4;
-	boxes.forEach((box, idx) => {
-		const boxTop = box.getBoundingClientRect().top;
+// function checkBoxes() {
+// 	const triggerBottom = window.innerHeight / 5 * 4;
+// 	boxes.forEach((box, idx) => {
+// 		const boxTop = box.getBoundingClientRect().top;
 		
-		if(boxTop < triggerBottom) {
-			box.classList.add('show');
-		} else {
-			box.classList.remove('show');
-		}
-	});
+// 		if(boxTop < triggerBottom) {
+// 			box.classList.add('show');
+// 		} else {
+// 			box.classList.remove('show');
+// 		}
+// 	});
+// }
+
+
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector(".nav_wrap").cloneNode(1);
+const logo = document.querySelector(".header_logo").cloneNode(1)
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
+
+// Выполняем действия при клике ..
+function hambHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+  popup.appendChild(menu);
+  popup.appendChild(logo)
+}
+
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
 }

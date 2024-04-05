82d654b9ac4c5840c57f5dd755139a17bc7fe7ce
Alesia_Shikurova <ales2503@yandex.by>
2024-04-05 23:36:50 +0300

    feat: fix nav

--- main.js
+++ main.js
@@ -85,3 +85,27 @@ function closeOnClick() {
   hamb.classList.remove("active");
   body.classList.remove("noscroll");
 }
+
+
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+      center: [55.753994, 37.622093],
+      zoom: 9,
+      // Добавим кнопку для построения маршрутов на карту.
+      controls: ['routeButtonControl']
+  });
+
+  var control = myMap.controls.get('routeButtonControl');
+
+  // Зададим координаты пункта отправления с помощью геолокации.
+  control.routePanel.geolocate('from');
+  
+  control.routePanel.state.set({
+      type: "pedestrian",
+      to: '55.659172, 37.762848'
+  });
+
+  // Откроем панель для построения маршрутов.
+  control.state.set('expanded', true);
+});
